292c781b34a0fad13eaaba0773ba410925a34e41
Joseph Chipperfield <joechip1709@gmail.com>
2021-01-08 10:13:01 +0000

    Feature updates and responsive elements configured
    Single page navigation added as well as the images/text becoming responsive. Also adjusted layout of certain containers to suit the device being viewed on

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-//burger menu functionality
-
-const burger = document.querySelector(".burger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll('.nav-links li');
-const landing = document.querySelector('.landing-page');
-
-burger.addEventListener('click', () => {
-  navLinks.classList.toggle("open");
-  landing.classList.toggle("open");
-  links.forEach( link => {
-    link.classList.toggle('fade')
-  });
-});
+//Function for extra text to be displayed on the client portfolio sections.
 
 const petText = document.getElementById("pet-text");
 const petInfoButton = document.getElementById("pet-info");
@@ -26,3 +13,44 @@ const photoInfoButton = document.getElementById("photo-info");
 photoInfoButton.addEventListener('click', () => {
   photoText.classList.toggle("open");
 });
+
+//Smooth scroll navigation for the homepage buttons to allow quick navigation.
+
+function smoothScroll(target, duration){
+  var target=document.querySelector(target);
+  var targetPosition = target.getBoundingClientRect().top;
+  var startPosition = window.pageYOffset;
+  var distance = targetPosition - startPosition;
+  var startTime = null;
+
+  function animation(currentTime){
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0,run);
+    if(timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  function ease(t, b, c, d) {
+    t /= d/2;
+    if (t < 1) return c/2*t*t + b;
+    t--;
+    return -c/2 * (t*(t-2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+var portfolioScroll = document.getElementById('portfolio-nav');
+var contactScroll = document.getElementById('contact-nav');
+var backToTop = document.getElementById('back-top');
+
+portfolioScroll.addEventListener('click', function(){
+smoothScroll('.portfolio', 1000);
+})
+contactScroll.addEventListener('click', function(){
+smoothScroll('.overall-contact-container', 1000);
+})
+backToTop.addEventListener('click', function(){
+smoothScroll('.landing-page', 1000);
+})
