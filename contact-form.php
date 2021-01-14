45373f70ed76755e718fdfa4b9b641db8f9a6ea2
Joseph Chipperfield <joechip1709@gmail.com>
2021-01-14 19:09:43 +0000

    small additions made to text errors

--- contact-form.php
+++ contact-form.php
@@ -2,9 +2,10 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "joechip1709@gmail.com";
+$recipient = "joe@joechipperfield.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader);
-header ('Location: contact-complete.php');
+header('Location: contact-complete.html');
+exit();
 ?>
